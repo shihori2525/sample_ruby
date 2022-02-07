4a9a47e48a8ac7c5d5e14e56a68c77ac2922013d
sakata shihori <rirakkuma.250@gmail.com>
2022-02-07 14:16:04 +0000

    [Add]greeting

--- if.rb
+++ if.rb
@@ -1,6 +1,18 @@
-data = find_data
-if data
-  puts 'データがあります'
+n = 11
+if n > 10
+   puts '10より大きい'
 else
-  puts 'データはありません'
+  puts '10以下'
 end
+
+country = 'italy'
+if country == 'japan'
+  puts 'こんにちは'
+elsif country == 'us'
+  puts 'Hello'
+elsif country == 'italy'
+  puts 'Cial'
+else
+  puts '???'
+end
+
